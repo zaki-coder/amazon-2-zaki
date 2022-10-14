bc312397f6f931e8f4479407625c40ac81fe3460
zaki-coder <zakiazizi1841992@gmail.com>
2022-10-14 03:41:59 +0200

    my amazon clone

---
+++ src/components/Banner.js
@@ -0,0 +1,29 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+const Banner = () => {
+  return (
+    <div className="relative">
+      <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20" />
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showIndicators={false}
+        showStatus={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div>
+          <img src="https://links.papareact.com/gi1" alt="" loading="lazy" />
+        </div>
+        <div>
+          <img src="https://links.papareact.com/6ff" alt="" loading="lazy" />
+        </div>
+        <div>
+          <img src="https://links.papareact.com/7ma" alt="" loading="lazy" />
+        </div>
+      </Carousel>
+    </div>
+  );
+}
+
+export default Banner
